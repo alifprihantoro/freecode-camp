f32a265a46a67bfc6a488308cdd5eea6ad451677
alifprihantoro <alifmuryp@gmail.com>
2022-07-03 06:47:12 +0700

    add drum challange starter

---
+++ drum/config/tailwind.config.js
@@ -0,0 +1,19 @@
+const PATH = 'drum'
+module.exports = {
+  content: [
+    PATH+'/src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          1: '#035a0e',
+          2: '#038811',
+          3: '#35a041',
+          3: '#b3f166',
+        },
+      },
+    },
+    plugins: [],
+  },
+}
